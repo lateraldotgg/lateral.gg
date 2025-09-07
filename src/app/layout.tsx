546e7c72e15a235146d7bb1f870273783e226c2e
no1canned <no1canned@lateral.gg>
2025-09-07 11:45:27 +1000

    Future proofing for smooth scroll.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head></head>
       <body
         className={`${oxanium.variable} ${shareTechMono.variable} antialiased overflow-hidden`}
